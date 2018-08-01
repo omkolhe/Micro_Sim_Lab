3c659855d5eab1cf650d29e4e048960fa1c40c15
omkolhe <omkolhe026@gmail.com>
2018-08-02 02:04:45 +0530

    Question1 matlab - very slow and no voltage plot generated

--- Assignment1/Q1.m
+++ Assignment1/Q1.m
@@ -1,15 +1,15 @@
 %%Dimensions of the plates
 %End points of Plate 1
-x1 = -10+60;
-y1 = -10+60;
-x2 = 60;
-y2 = -10+60;
+x1 = -5+600;
+y1 = -500+600;
+x2 = 5+600;
+y2 = -500+600;
 
-l1=20;
-l2=10;
+l1=1000;
+l2=1000;
 %The capacitor is placed on a bigger box of size N1xN2
-Nx = 6*l1;
-Ny = 12*l2;
+Nx = 1200;
+Ny = 1200;
 x = linspace(1,Nx,Nx);
 y = linspace(1,Ny,Ny);      
 
@@ -17,16 +17,16 @@ y = linspace(1,Ny,Ny);
 
 V = zeros(Nx,Ny);
 
-V(x1,y1:y1+l1) = 0;
-V(x2:x2+l2,y2) = 1;
+V(x1,y1:y1+l1) = 99999;
+V(x2,y2:y2+l2) = 0;
 
-iter=3000;
+iter=10;
 
 count=0;
 while count<iter
     for i = 2:Nx-1 %x
         for j = 2:Ny-1 %y
-            if (i>=x2 && i<x2+l2 && j==y2)
+            if (j>=y2 && j<y2+l2 && i==x2)
                 %print(i,j,V[i][j])
                 continue
             end
@@ -37,7 +37,7 @@ while count<iter
             V(i,j)=(V(i-1,j)+V(i+1,j)+V(i,j-1)+V(i,j+1))/4;
         end
     end
-    count=count+1;
+    count=count+1
 end
 
 surf(X,Y,V)
